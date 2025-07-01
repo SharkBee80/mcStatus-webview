3e1b3101291d46c09a322a1067823a1f85742b0d
SharkBee80 <115618339+SharkBee80@users.noreply.github.com>
2025-07-01 16:46:28 +0800

    feat(backend): 添加定时器功能
    - 新增 timer_ 类，用于实现定时任务
    - 提供了初始化方法和任务调度方法- 支持取消之前的定时任务并重新开始计时
    - 使用守护线程，避免阻塞主程序退出

---
+++ src/backend/timer.py
@@ -0,0 +1,19 @@
+from threading import Timer
+
+
+class timer_:
+    def __init__(self, delay: float):
+        """
+
+        :param delay: s
+        """
+        self.delay = delay
+        self.timer = None
+
+    def task(self, callback, *args):
+        """每次调用会取消之前的定时任务，并重新开始计时"""
+        if self.timer:
+            self.timer.cancel()
+        self.timer = Timer(self.delay, callback, args)
+        self.timer.daemon = True  # 守护线程，避免阻塞主程序退出
+        self.timer.start()
